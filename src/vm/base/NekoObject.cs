f81167ad76c9b0b67c6801cb92f82411b139a51a
Yuuki Wesp <ls-micro@ya.ru>
2020-08-20 03:38:54 +0300

    add GetHashCode

--- src/vm/base/NekoObject.cs
+++ src/vm/base/NekoObject.cs
@@ -1,9 +1,12 @@
 ﻿namespace Neko.Base
 {
+    using NativeRing;
+
     public unsafe class NekoObject : NekoBehaviour
     {
         protected internal NekoObject(NekoValue* value) : base(value) { }
 
         public override string ToString() => $"NekoObject {@ref->GetValueType()}";
+        public override int GetHashCode() => Native.neko_val_hash(this.@ref);
     }
 }
