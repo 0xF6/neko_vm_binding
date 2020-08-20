fc7663f41f02e6ef23f4c640f5c406efcd6601fd
Yuuki Wesp <ls-micro@ya.ru>
2020-08-21 02:52:40 +0300

    add indexer accessor to functions

--- src/vm/base/NekoModule.cs
+++ src/vm/base/NekoModule.cs
@@ -5,5 +5,7 @@
     public sealed unsafe class NekoModule : NekoBehaviour
     {
         internal NekoModule(NekoValue* value) : base(value) { }
+
+        public NekoFunction this[string name] => NekoFunction.Create(this, name);
     }
 }
