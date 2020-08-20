58febe92b6dcfd4f1b81c1a5f2f0ef9fd9f3b7f9
Yuuki Wesp <ls-micro@ya.ru>
2020-08-21 02:53:42 +0300

    bump

--- src/host/Program.cs
+++ src/host/Program.cs
@@ -1,18 +1,27 @@
 ﻿namespace neko.host
 {
+    using System;
     using System.IO;
     using System.Runtime.InteropServices;
     using Neko;
     using Neko.Base;
+    using Neko.NativeRing;
 
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void tc()
+        {
+            Console.WriteLine("ITS WORK");
+        }
+        public static unsafe void Main(string[] args)
         {
             using var vm = new Neko();
-
             var module = vm.LoadModule(new FileInfo("test.n"));
-            NekoFunction.Create(module, "log").Invoke();
+
+            var f1 = module["assert_int"].Invoke<int>(1);
+            var f2 = module["assert_string"].Invoke<string>("xuy");
+            var f3 = module["assert_boolean"].Invoke<bool>(true);
+            var f4 = module["assert_function"].Invoke<NekoFunction>(NekoFunction.Create(tc, nameof(tc))).Invoke();
         }
     }
 }
