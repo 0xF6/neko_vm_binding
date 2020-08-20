c1d19d7fe7ec724d13a2da4690ccc57b1dc393bb
Yuuki Wesp <ls-micro@ya.ru>
2020-08-20 03:38:29 +0300

    add IsNotAFunctionNekoException, TypeIsNotSupportNekoException

--- src/vm/base/NekoException.cs
+++ src/vm/base/NekoException.cs
@@ -9,9 +9,19 @@
         protected NekoException(string message) : base(message) { }
     }
 
-    public sealed class NekoModuleLoadException : NekoException
+    public sealed class ModuleLoadNekoException : NekoException
     {
-        public NekoModuleLoadException(FileInfo file, string exception) 
+        public ModuleLoadNekoException(FileInfo file, string exception) 
             : base($"Failed load module '{file.Name}' due to '{exception}'.") { }
+    }
+    public sealed class IsNotAFunctionNekoException : NekoException
+    {
+        public IsNotAFunctionNekoException(string functionName) 
+            : base($"'{functionName}' is not a function.") { }
+    }
+    public sealed class TypeIsNotSupportNekoException : NekoException
+    {
+        public TypeIsNotSupportNekoException(string ty) 
+            : base($"'{ty}' is support type.") { }
     }
 }
