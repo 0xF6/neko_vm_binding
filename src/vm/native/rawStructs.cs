ab8b0549c9331637195565a86c749d65418a2a28
Yuuki Wesp <ls-micro@ya.ru>
2020-08-20 03:39:08 +0300

    add structs

---
+++ src/vm/native/rawStructs.cs
@@ -0,0 +1,33 @@
+﻿namespace Neko.NativeRing
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public unsafe struct hcell
+    {
+        public int hkey;
+        public NekoValue key;
+        public NekoValue val;
+        public hcell* next;
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public unsafe struct vhash
+    {
+        public hcell** cells;
+        public int ncells;
+        public int nitems;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public ref struct vint32
+    {
+        public uint t;
+        public int i;
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public ref struct vfloat
+    {
+        public uint t;
+        public double f;
+    }
+}
