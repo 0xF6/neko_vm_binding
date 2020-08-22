ff8c636c9e7dcadc15f3a6a9a5521b8004bbb8ca
Yuuki Wesp <ls-micro@ya.ru>
2020-08-22 08:01:58 +0300

    add native wrap in NekoModule

--- src/vm/base/NekoModule.cs
+++ src/vm/base/NekoModule.cs
@@ -7,5 +7,30 @@
         internal NekoModule(NekoValue* value) : base(value) { }
 
         public NekoFunction this[string name] => NekoFunction.Create(this, name);
+
+        public __module* AsInternal() => (__module*) @ref;
+
+        public struct __module
+        {
+            public void* jit;
+            public uint nglobals;
+            public uint nfields;
+            public uint codesize;
+            public NekoValue* name;
+            public NekoValue** globals;
+            public NekoValue** fields;
+            public NekoValue* loader;
+            public NekoValue* exports;
+            public NekoValue* jit_gc;
+            public NekoValue* dbgtbl;
+            public NekoDebug* dbgidxs;
+            public void* code;
+        }
+
+        public struct NekoDebug
+        {
+            public int @base;
+            public uint bits;
+        }
     }
 }
