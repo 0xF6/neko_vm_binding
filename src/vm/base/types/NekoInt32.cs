37f11a20e1a73e42c531da548d97759aec719427
Yuuki Wesp <ls-micro@ya.ru>
2020-08-23 10:35:40 +0300

    add NekoInt32 impl

---
+++ src/vm/base/types/NekoInt32.cs
@@ -0,0 +1,24 @@
+﻿namespace Neko.Base
+{
+    using System;
+    using NativeRing;
+
+    public sealed unsafe class NekoInt32 : NekoObject, INativeCast<_neko_int32>
+    {
+        internal NekoInt32(NekoValue* value) : base(value) 
+            => NekoAssert.IsInt32(value);
+        public _neko_int32* AsInternal() => (_neko_int32*) @ref;
+
+        public int GetValue() => this;
+
+        public static implicit operator int(NekoInt32 i)
+        {
+            if (NekoType.is_null(i.@ref))
+                throw new NullReferenceNekoException<int>(i.@ref);
+            return i.AsInternal()->i;
+        }
+
+        public static implicit operator NekoInt32(int i) => 
+            new NekoInt32(Native.neko_alloc_int32(i));
+    }
+}
