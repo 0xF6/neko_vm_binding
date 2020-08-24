60a489a837838c1c4ebac635f2f3b9544bc92b87
Yuuki Wesp <ls-micro@ya.ru>
2020-08-24 05:43:21 +0300

    add range extensions

---
+++ src/vm/extensions/RangeEx.cs
@@ -0,0 +1,12 @@
+﻿namespace Neko
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class RangeEx
+    {
+        public static IEnumerable<T> ForEach<T>(this Range range, Func<int,  T> functor) 
+            => Enumerable.Range(range.Start.Value, range.End.Value - range.Start.Value).Select(functor);
+    }
+}
