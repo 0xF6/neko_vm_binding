2d679e3c672e77579f28eacc59331a86bfc1c8b5
Yuuki Wesp <ls-micro@ya.ru>
2020-08-24 05:50:35 +0300

    add NekoBool

---
+++ src/vm/base/types/NekoBool.cs
@@ -0,0 +1,15 @@
+﻿namespace Neko.Base
+{
+    using NativeRing;
+
+    public sealed unsafe class NekoBool : NekoObject
+    {
+        internal NekoBool(NekoValue* value) : base(value) => NekoAssert.IsBool(value);
+
+
+        public bool Value => this.@ref == Native.v_true();
+
+        public static implicit operator bool(NekoBool v) => v.Value;
+        public static implicit operator NekoBool(bool v) => v ? new NekoBool(Native.v_true()) : new NekoBool(Native.v_false());
+    }
+}
