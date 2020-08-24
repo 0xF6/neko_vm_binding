a307ce7dff775986192a42c1c05d3c0f5c9b1b18
Yuuki Wesp <ls-micro@ya.ru>
2020-08-24 05:54:37 +0300

    rework runtime object

--- src/vm/base/types/NekoRuntimeObject.cs
+++ src/vm/base/types/NekoRuntimeObject.cs
@@ -1,31 +1,63 @@
 ﻿namespace Neko.Base
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Dynamic;
+    using System.Linq;
     using NativeRing;
-    public unsafe delegate void Extract(NekoValue* data, int id, NekoValue** _);
-    public sealed unsafe class NekoRuntimeObject : NekoObject, INativeCast<_runtime_obj>
+    public sealed unsafe class NekoRuntimeObject : NekoObject, INativeCast<_runtime_obj>, IEnumerable<NekoObject>
     {
-        internal NekoRuntimeObject(NekoValue* value) : base(value) 
-            => NekoAssert.IsRuntimeObject(value);
-
-        public int FieldsCount => (&AsInternal()->table)->count;
-        public NekoValue* GetFields()
+        private DynamicNekoObjectProxy proxy { get; }
+        internal NekoRuntimeObject(NekoValue* value) : base(value)
         {
-            var array = new NekoArray(Native.neko_alloc_array((uint)FieldsCount));
-            Iterator(&AsInternal()->table, rec, &array.AsInternal()->ptr);
-            return array;
-        }
-        public static void rec(NekoValue* d, int id, void**array)
-        {
-            **array++ = ((IntPtr)id);
+            NekoAssert.IsRuntimeObject(value);
+            proxy = new DynamicNekoObjectProxy(this);
         }
 
-        public void Iterator(_neko_objtable *t, Extract extractor, NekoValue** p) 
-        {
-            var c = t->cells;
-            for (var i = 0; i < t->count; i++)
-                extractor(c[i].v, c[i].id, p);
-        }
+        public NekoObject this[string name] 
+            => Native.neko_val_field(@ref, Native.neko_val_id(name));
+
+        public dynamic AsDynamic() => proxy;
+
         public _runtime_obj* AsInternal() => (_runtime_obj*) @ref;
+
+        public string[] GetFields() =>
+            (..AsInternal()->table.count)
+            .ForEach(x => *&((_runtime_obj*) @ref)->table.cells[x * sizeof(short)])
+            .Select(x => (NekoObject)Native.neko_val_field_name(x.id))
+            .Cast<NekoString>()
+            .Select(x => x.Value)
+            .ToArray();
+
+
+
+        public class DynamicNekoObjectProxy : DynamicObject
+        {
+            private readonly NekoRuntimeObject _obj;
+
+            internal DynamicNekoObjectProxy(NekoRuntimeObject obj) => _obj = obj;
+
+            public override IEnumerable<string> GetDynamicMemberNames() => _obj.GetFields();
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                try
+                {
+                    result = (NekoObject)Native.neko_val_field(_obj.@ref, Native.neko_val_id(binder.Name));
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine(e);
+                    result = null;
+                    return false;
+                }
+            }
+        }
+        public IEnumerator<NekoObject> GetEnumerator() 
+            => GetFields().Select(field => this[field]).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
