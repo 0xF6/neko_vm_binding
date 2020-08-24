8e541cd7de66c3af925c8008b699fe07219f0a24
Yuuki Wesp <ls-micro@ya.ru>
2020-08-24 06:05:53 +0300

    rework setter

--- src/vm/base/types/NekoArray.cs
+++ src/vm/base/types/NekoArray.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     using NativeRing;
     
     public unsafe class NekoArray : NekoObject, IEnumerable<NekoObject>, INativeCast<_neko_array>, ICollection
@@ -14,42 +15,17 @@
             get => GetByIndex(i);
             set => SetByIndex(i, value);
         }
-        public void SetByIndex(int index, NekoObject obj) 
-            => SetByIndexNative(index, obj);
         public NekoObject GetByIndex(int index) 
             => GetByIndexNative(index);
+        public void SetByIndex(int index, NekoObject value) 
+            => SetByIndexNative(index, value.@ref);
         public NekoValue* GetByIndexNative(int index) 
-            => &((_neko_array*)@ref)->ptr[index];
-        public void SetByIndexNative(int index, NekoValue* value)
-        {
-            var ptr = (void**) &((_neko_array*) @ref)->ptr;
-            ptr[index] = value;
-        }
+            => (NekoObject)(&((_neko_array*) @ref)->ptr[index * sizeof(short)]);
+        public void SetByIndexNative(int index, NekoValue* value) 
+            => ((void**) &((_neko_array*) @ref)->ptr)[index * sizeof(short)] = value;
 
         public _neko_array* AsInternal() 
             => (_neko_array*)@ref;
-
-        #region Implementation of IEnumerable
-
-        public IEnumerator<NekoObject> GetEnumerator()
-        {
-            for (var i = 0; i != Native.neko_val_array_size(this); i++)
-                yield return GetByIndex(i);
-        }
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        #endregion
-
-        #region Implementation of ICollection
-
-        public void CopyTo(Array array, int index) 
-            => throw new NotSupportedException();
-
-        public int Count => Native.neko_val_array_size(this);
-        public bool IsSynchronized => false;
-        public object SyncRoot { get; } = new object();
-
-        #endregion
 
         public static NekoArray Alloc(int size) => Alloc((uint) size);
         public static NekoArray Alloc(uint size)
@@ -60,5 +36,24 @@
                 darr[i] = Native.v_null();
             return darr;
         }
+
+        #region 
+
+        public IEnumerator<NekoObject> GetEnumerator()
+        {
+            for (var i = 0; i != Native.neko_val_array_size(this); i++)
+                yield return GetByIndex(i);
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+
+        public void CopyTo(Array array, int index) 
+            => throw new NotSupportedException();
+
+        public int Count => Native.neko_val_array_size(this);
+        public bool IsSynchronized => false;
+        public object SyncRoot { get; } = new object();
+
+        #endregion
     }
 }
