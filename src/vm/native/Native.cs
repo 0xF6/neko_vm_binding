c17fa8241b8315fa86f25d30f7ae75e4dbfa6ac7
Yuuki Wesp <ls-micro@ya.ru>
2020-08-25 08:34:11 +0300

    add neko_alloc_int31

--- src/vm/native/Native.cs
+++ src/vm/native/Native.cs
@@ -136,6 +136,9 @@
         [DllImport("neko")]
         public static extern NekoValue* neko_alloc_int32(int v);
 
+        public static NekoValue* neko_alloc_int31(int v) 
+            => ((NekoValue*) (IntPtr) ((((int) (v)) << 1) | 1));
+
 
         public static NekoValue* neko_alloc_bool(bool b) => b ? v_true() : v_false();
         public static NekoValue* neko_alloc_int(int v) => (NekoValue*) (IntPtr) ((v << 1) | 1);
