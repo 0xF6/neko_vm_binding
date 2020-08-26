277deaf290fd2dd50880a4681111a2862d248668
Yuuki Wesp <ls-micro@ya.ru>
2020-08-26 19:17:55 +0300

    use usings

--- src/vm/tag.cs
+++ src/vm/tag.cs
@@ -1,2 +1,4 @@
-﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("vm-base-test")]
+﻿using System;
+using System.Runtime.InteropServices;
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("vm-base-test")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("host")]
