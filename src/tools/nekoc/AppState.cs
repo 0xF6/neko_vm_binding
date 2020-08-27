2aaa1cfd6b8e0aac645a232bc2b467f2388743c4
Yuuki Wesp <ls-micro@ya.ru>
2020-08-27 08:20:09 +0300

    total refactoring

---
+++ src/tools/nekoc/AppState.cs
@@ -0,0 +1,46 @@
+﻿namespace nekoc
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Logging.Console;
+    using static System.Environment.SpecialFolder;
+    using static System.Environment;
+    using static System.IO.Path;
+    public class AppState
+    {
+        public static bool isTrace = false;
+        public static bool forceInstall = false;
+        public static ILogger<AppState> logger { get; private set; }
+
+
+        public static DirectoryInfo GetFolderForCache() =>
+            new(Combine(Combine(GetFolderPath(ApplicationData), "dotnet-nekoc-cache"), GetVersion()));
+
+        public static string GetOS()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return "win";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return "linux";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return "osx";
+            throw new NotSupportedException();
+        }
+
+        public static string GetVersion() =>
+            Assembly.GetEntryAssembly()?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion ?? "any";
+
+
+        public static void SetupLogger() =>
+            logger = LoggerFactory.Create(x => x
+                .AddConsoleFormatter<NekoConsoleFormatter, ConsoleFormatterOptions>()
+                .AddConsole(x => x.FormatterName = nameof(NekoConsoleFormatter))).CreateLogger<AppState>();
+
+        private ILoggerFactory factory;
+    }
+}
--- src/tools/nekoc/Program.cs
+++ src/tools/nekoc/Program.cs
@@ -1,126 +1,129 @@
-﻿namespace nekoc
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Pastel;
+using System.IO.Compression;
+using Microsoft.Extensions.Logging;
+using MoreLinq;
+using nekoc;
+using static System.Console;
+using static System.Environment;
+using static System.Environment.SpecialFolder;
+using static System.IO.Path;
+using static nekoc.AppState;
+
+const string NEKO_C_VERSION = "2.3.0";
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    OutputEncoding = Encoding.Unicode;
+SetupLogger();
+PopArgs(ref args, "--trace", ref isTrace);
+PopArgs(ref args, "--force-install", ref forceInstall);
+
+if(isTrace)
+    trace("trace log enabled");
+if(forceInstall)
+    trace("force installing enabled");
+
+PrintHeader();
+
+if (GetOS() != "win")
 {
-    using System;
-    using System.Diagnostics;
-    using System.Drawing;
-    using System.IO;
-    using System.Linq;
-    using System.Reflection;
-    using System.Runtime.InteropServices;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Pastel;
-    using static System.Console;
-    using static System.Environment;
-    using static System.Environment.SpecialFolder;
-    using static System.IO.Path;
-
-    internal class Program
-    {
-        public const string AssemblyKey = "nekoc";
-        public const string CompilersFolder = "resources.binaries";
-
-        public static string GetPathCompilerForCurrentOS()
-        {
-            var result = $"{AssemblyKey}.{CompilersFolder}.{GetOS()}_x64.nekoc";
-            if (GetOS() == "win")
-                result = $"{result}.exe";
-            return result;
-        }
-
-        public static Stream GetCompilerContent() => 
-            typeof(Program)
-                .GetTypeInfo()
-                .Assembly
-                .GetManifestResourceStream(GetPathCompilerForCurrentOS());
-
-        private static string GetOS()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                return "win";
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return "linux";
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                return "osx";
-            throw new NotSupportedException();
-        }
-
-        public static string GetVersion() =>
-            Assembly.GetEntryAssembly()?
-                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                .InformationalVersion ?? "any";
-
-        public static DirectoryInfo GetFolderForCache() =>
-            new(Combine(Combine(GetFolderPath(ApplicationData), "dotnet-nekoc-cache"), GetVersion()));
-
-        public static FileInfo GetCompilerBinariesFile()
-        {
-            var cache = GetFolderForCache();
-            if (!cache.Exists)
-                Directory.CreateDirectory(cache.FullName);
-            return new(Combine(cache.FullName, $"nekoc{(GetOS() == "win" ? ".exe" : "")}"));
-        }
-
-        public static async Task<FileInfo> GetOrCreateCompilerAsync()
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                OutputEncoding = Encoding.Unicode;
-
-            PrintHeader();
-            if (GetOS() != "osx")
-            {
-                WriteLine($"Please note that the tool may not work correctly under the OSX operating system.".Pastel(Color.Orange));
-                WriteLine($"If you find a problem, report it to the repository.".Pastel(Color.Orange));
-            }
-
-            var result = GetCompilerBinariesFile();
-
-            if (result.Exists)
-                return result;
-
-            await using var stream = GetCompilerContent();
-            await using var memory = new MemoryStream();
-            await stream.CopyToAsync(memory);
-            await File.WriteAllBytesAsync(result.FullName, memory.ToArray());
-
-            return result;
-        }
-
-        private static void PrintHeader()
-        {
-            if (GetEnvironmentVariable("WT_SESSION") == null && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                SetEnvironmentVariable($"NO_COLOR", "true");
-                return;
-            }
-            WriteLine($"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⡀⢳⣤⡙⠻⢿⣿⣿⣿⠿⢿⣿⣿⠿⢿⣿⣿⣿⠿⠛⣡⣴⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣧⠸⡀⡙⢶⣤⣈⣁⣤⣤⠀⠹⠃⢴⣶⣤⣭⣤⡶⢋⡁⡜⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣆⢻⡼⣷⡍⠉⠁⠀⣹⣆⠀⢀⣿⡁⠀⠉⠉⣴⡏⣼⢃⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⡿⢀⠇⠉⠀⠀⠀⠘⠛⢻⣦⡾⠛⠋⠀⠀⠀⠈⠁⣧⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⡿⢁⡞⠀⠀⣴⡶⠦⣄⡀⠿⣿⡿⠂⣠⠴⠶⣶⡀⠀⠸⣧⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⠃⣼⢀⣤⣶⣿⣿⣶⣶⣿⣶⣿⣷⣿⣷⣶⣾⣿⣶⣤⡀⢹⡆⢿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⡿⢿⣿⣿⡿⠿⠿⠿⠿⠿⣿⣿⣿⠿⣿⣿⣿⠿⢿⣿⣿⡿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⢰⠃⣠⣿⣿⠿⠿⠟⠿⢿⡇⠀⠀⣹⠿⠛⠛⠻⠿⢿⣇⠈⢷⢸⣿⣿⣿⣿⣿⣿⡇⢠⣄⠙⢿⡇⢸⣿⣿⡗⠒⠒⠒⠒⠒⣿⣿⣿⠀⠛⠛⠃⠐⢻⣿⣿⡇⢰⣶⣶⣶⠀⣿⣿⣿⣿⣿\r\n⣿⣿⡏⢸⡾⠛⢉⣤⣤⣄⠀⠶⠿⠟⠀⠘⠛⠿⠶⢀⣤⣤⣀⠈⠳⣾⠀⣿⣿⣿⣿⣿⣿⣇⣸⣿⣷⣤⣀⣸⣿⣿⣏⣉⣉⣉⣉⣉⣿⣿⣿⣀⣿⣿⣿⣇⣸⣿⣿⣇⣈⣉⣉⣉⣀⣿⣿⣿⣿⣿\r\n⣿⣿⡇⠈⠀⣼⠟⠿⣿⣿⣿⣶⠀⢰⠀⢠⡆⢰⣾⣿⣿⡿⠟⢿⣆⠈⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣇⠀⢰⣏⣴⡾⠟⠛⠛⠛⣇⢸⣿⣿⢇⡟⠛⠛⠛⠿⢷⣄⣿⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣧⠀⠟⠁⣤⣾⣿⣿⣧⠸⣾⣭⣽⣾⢠⣿⣿⣿⣶⣄⠙⠏⢠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣧⠀⣾⣿⣿⣿⣿⣿⣆⠙⠿⠿⢃⣼⣿⣿⣿⣿⣿⡆⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
-                .Pastel(Color.MediumPurple));
-            WriteLine();
-        }
-
-        public static async Task<int> Main(string[] args)
-        {
-            var result = await GetOrCreateCompilerAsync();
-
-            if (args is { Length: 0 })
-                return await HelpAsync(result);
-            return await ExecAsync(result, args.Concat(new []{ "-v" }).ToArray());
-        }
-
-        private static Task<int> HelpAsync(FileInfo compiler) =>
-            ExecAsync(compiler, "--help", "-v");
-
-        public static async Task<int> ExecAsync(FileInfo compiler, params string[] args)
-        {
-            var inf = new ProcessStartInfo(compiler.FullName, string.Join(' ', args));
-            inf.WorkingDirectory = Directory.GetCurrentDirectory();
-            var proc = Process.Start(inf);
-            await proc.WaitForExitAsync();
-
-            if (proc.ExitCode == 0)
-                WriteLine($"Success.".Pastel(Color.GreenYellow));
-            return proc.ExitCode;
-        }
-    }
+    WriteLine($"Temporarily this tool can only run on the Windows operating system.".Pastel(Color.Orange));
+    WriteLine($"Due to some problems when working with tar archives".Pastel(Color.Orange));
+    return -1;
 }
+
+if (GetOS() == "osx")
+{
+    WriteLine($"Please note that the tool may not work correctly under the OSX operating system.".Pastel(Color.Orange));
+    WriteLine($"If you find a problem, report it to the repository.".Pastel(Color.Orange));
+}
+
+var compilerFile = await GetOrCreateCompilerAsync();
+
+trace($"call {nameof(GetOrCreateCompilerAsync)} -> {compilerFile}");
+
+if (args is { Length: 0 })
+    return await ExecAsync(compilerFile, "--help", "-v");
+return await ExecAsync(compilerFile, args.Concat(new []{ "-v" }).ToArray());
+
+
+
+
+static FileInfo GetCompilerBinariesFile()
+{
+    var cache = GetFolderForCache();
+    if (!cache.Exists)
+        Directory.CreateDirectory(cache.FullName);
+    return new(Combine(cache.FullName, $"nekoc{(GetOS() == "win" ? ".exe" : "")}"));
+}
+
+async Task<FileInfo> GetOrCreateCompilerAsync()
+{
+    var cache = GetFolderForCache();
+    if (forceInstall && cache.Exists)
+    {
+        cache.EnumerateFiles("*.*").Pipe(x => x.Delete()).Pipe(x => trace($"delete '{x}'..")).Consume();
+        cache.EnumerateDirectories().Pipe(x => x.Delete()).Pipe(x => trace($"delete '{x}'..")).Consume();
+    }
+    var result = GetCompilerBinariesFile();
+
+    if (result.Exists)
+        return result;
+    Action<string> extractor = null;
+    var zip = await new GithubClient("HaxeFoundation", "neko", NEKO_C_VERSION).DownloadAsync();
+    if (GetOS() == "win")
+        extractor = (s) => ZipFile.ExtractToDirectory(zip.FullName, s);
+    else
+        throw new NotSupportedException(); // fucking tar archive and fucking poxis pex
+
+    extractor(GetFolderForCache().FullName);
+
+    var targetDir = GetFileNameWithoutExtension(zip.FullName);
+    var extractedDir = Combine(cache.FullName, targetDir);
+
+    foreach (var file in Directory.GetFiles(Combine(cache.FullName, targetDir), "*.*", SearchOption.AllDirectories))
+        File.Move(file, Combine(cache.FullName, GetFileName(file)));
+    Directory.Delete(extractedDir, true);
+    return result;
+}
+
+void PrintHeader()
+{
+    if (GetEnvironmentVariable("WT_SESSION") == null && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        trace("disable coloring and emoji...");
+        SetEnvironmentVariable($"NO_COLOR", "true");
+        SetEnvironmentVariable($"EMOJI_USE", "0");
+        return;
+    }
+    WriteLine($"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⡀⢳⣤⡙⠻⢿⣿⣿⣿⠿⢿⣿⣿⠿⢿⣿⣿⣿⠿⠛⣡⣴⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣧⠸⡀⡙⢶⣤⣈⣁⣤⣤⠀⠹⠃⢴⣶⣤⣭⣤⡶⢋⡁⡜⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣆⢻⡼⣷⡍⠉⠁⠀⣹⣆⠀⢀⣿⡁⠀⠉⠉⣴⡏⣼⢃⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⡿⢀⠇⠉⠀⠀⠀⠘⠛⢻⣦⡾⠛⠋⠀⠀⠀⠈⠁⣧⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⡿⢁⡞⠀⠀⣴⡶⠦⣄⡀⠿⣿⡿⠂⣠⠴⠶⣶⡀⠀⠸⣧⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⠃⣼⢀⣤⣶⣿⣿⣶⣶⣿⣶⣿⣷⣿⣷⣶⣾⣿⣶⣤⡀⢹⡆⢿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⡿⢿⣿⣿⡿⠿⠿⠿⠿⠿⣿⣿⣿⠿⣿⣿⣿⠿⢿⣿⣿⡿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⢰⠃⣠⣿⣿⠿⠿⠟⠿⢿⡇⠀⠀⣹⠿⠛⠛⠻⠿⢿⣇⠈⢷⢸⣿⣿⣿⣿⣿⣿⡇⢠⣄⠙⢿⡇⢸⣿⣿⡗⠒⠒⠒⠒⠒⣿⣿⣿⠀⠛⠛⠃⠐⢻⣿⣿⡇⢰⣶⣶⣶⠀⣿⣿⣿⣿⣿\r\n⣿⣿⡏⢸⡾⠛⢉⣤⣤⣄⠀⠶⠿⠟⠀⠘⠛⠿⠶⢀⣤⣤⣀⠈⠳⣾⠀⣿⣿⣿⣿⣿⣿⣇⣸⣿⣷⣤⣀⣸⣿⣿⣏⣉⣉⣉⣉⣉⣿⣿⣿⣀⣿⣿⣿⣇⣸⣿⣿⣇⣈⣉⣉⣉⣀⣿⣿⣿⣿⣿\r\n⣿⣿⡇⠈⠀⣼⠟⠿⣿⣿⣿⣶⠀⢰⠀⢠⡆⢰⣾⣿⣿⡿⠟⢿⣆⠈⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣇⠀⢰⣏⣴⡾⠟⠛⠛⠛⣇⢸⣿⣿⢇⡟⠛⠛⠛⠿⢷⣄⣿⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣧⠀⠟⠁⣤⣾⣿⣿⣧⠸⣾⣭⣽⣾⢠⣿⣿⣿⣶⣄⠙⠏⢠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣧⠀⣾⣿⣿⣿⣿⣿⣆⠙⠿⠿⢃⣼⣿⣿⣿⣿⣿⡆⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\r\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
+        .Pastel(Color.MediumPurple));
+    WriteLine();
+}
+static void PopArgs(ref string[] args, string arg, ref bool target)
+{
+    if (target = args.Contains(arg))
+        args = args.Where(x => x != arg).ToArray();
+}
+async Task<int> ExecAsync(FileInfo compiler, params string[] args)
+{
+    trace($"call {nameof(ExecAsync)} -> {compiler} and {string.Join(',', args)}");
+    var inf = new ProcessStartInfo(compiler.FullName, string.Join(' ', args));
+    inf.WorkingDirectory = Directory.GetCurrentDirectory();
+    var proc = Process.Start(inf);
+    await proc.WaitForExitAsync();
+    trace($"process '{compiler}' has complete execution with {proc.ExitCode} exit code");
+    if (proc.ExitCode == 0)
+        WriteLine($"success".Pastel(Color.GreenYellow));
+    return proc.ExitCode;
+}
+
+void trace(string s) => logger.LogTrace(s);
