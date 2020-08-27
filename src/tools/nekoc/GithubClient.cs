c2531aa447cd33fff4f64c21e1c94a935df51a4a
Yuuki Wesp <ls-micro@ya.ru>
2020-08-27 08:19:44 +0300

    add downloader from github

---
+++ src/tools/nekoc/GithubClient.cs
@@ -0,0 +1,122 @@
+﻿using Flurl.Http;
+
+namespace nekoc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Konsole;
+    using Newtonsoft.Json;
+
+    public class GithubClient
+    {
+        private readonly string _owner;
+        private readonly string _repo;
+        private readonly string _version;
+
+        public GithubClient(string owner, string repo, string version)
+        {
+            _owner = owner;
+            _repo = repo;
+            _version = version;
+        }
+
+        public string GetFile()
+        {
+            if (AppState.GetOS() == "win")
+                return $"neko-{_version}-{AppState.GetOS()}64.zip";
+            return $"neko-{_version}-{AppState.GetOS()}64.tar.gz";
+        }
+
+        public async ValueTask<FileInfo> DownloadAsync()
+        {
+            var releases = await $"https://api.github.com/repos/{_owner}/{_repo}/releases"
+                .WithHeader("User-Agent", $"NekoC .NET/{AppState.GetVersion()}")
+                .GetJsonAsync<GithubRelease[]>();
+            var release = releases.First();
+            var targetFile = GetFile();
+
+            var asset = release.Assets.FirstOrDefault(x => x.Name == targetFile);
+
+            if(asset is null)
+                throw new Exception($"Failed find {targetFile} in latest release in '{_owner}/{_repo}'");
+
+
+            using var handler = HttpClientDownloadWithProgress.Create(asset.BrowserDownloadUrl, 
+                new FileInfo(Path.Combine(AppState.GetFolderForCache().FullName, targetFile)));
+            Console.WriteLine($"{":page_with_curl:".Emoji()} Download {asset.BrowserDownloadUrl}..");
+            var pb = new ProgressBar(100, 0, '=');
+
+            handler.ProgressChanged += (size, downloaded, percentage) =>
+            {
+                if(percentage != null)
+                    pb.Refresh((int)percentage.Value, $"");
+            };
+
+            await handler.StartDownload();
+
+
+            Console.WriteLine();
+
+            return new FileInfo(Path.Combine(AppState.GetFolderForCache().FullName, targetFile));
+        }
+    }
+    public class GithubRelease
+    {
+        [JsonProperty("tag_name")]
+        public string TagName { get; set; }
+
+        [JsonProperty("target_commitish")]
+        public string TargetCommitish { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonProperty("published_at")]
+        public DateTimeOffset PublishedAt { get; set; }
+
+        [JsonProperty("assets")]
+        public List<Asset> Assets { get; set; }
+    }
+
+    public class Asset
+    {
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("node_id")]
+        public string NodeId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("content_type")]
+        public string ContentType { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        [JsonProperty("download_count")]
+        public long DownloadCount { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonProperty("browser_download_url")]
+        public string BrowserDownloadUrl { get; set; }
+    }
+}
---
+++ src/tools/nekoc/HttpClientDownloadWithProgress.cs
@@ -0,0 +1,95 @@
+﻿namespace nekoc
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class HttpClientDownloadWithProgress : IDisposable
+    {
+        private readonly string _downloadUrl;
+        private readonly string _destinationFilePath;
+
+        private HttpClient _httpClient;
+
+        public delegate void ProgressChangedHandler(long? totalFileSize, long totalBytesDownloaded,
+            double? progressPercentage);
+
+        public event ProgressChangedHandler ProgressChanged;
+
+        public HttpClientDownloadWithProgress(string downloadUrl, string destinationFilePath)
+        {
+            _downloadUrl = downloadUrl;
+            _destinationFilePath = destinationFilePath;
+        }
+
+
+        public static HttpClientDownloadWithProgress Create(string downloadUrl, FileInfo destination)
+        {
+            return new HttpClientDownloadWithProgress(downloadUrl, destination.FullName);
+        }
+
+        public async Task StartDownload()
+        {
+            _httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+
+            using var response = await _httpClient.GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead);
+            await DownloadFileFromHttpResponseMessage(response);
+        }
+
+        private async Task DownloadFileFromHttpResponseMessage(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+
+            var totalBytes = response.Content.Headers.ContentLength;
+
+            await using var contentStream = await response.Content.ReadAsStreamAsync();
+            await ProcessContentStream(totalBytes, contentStream);
+        }
+        private async Task ProcessContentStream(long? totalDownloadSize, Stream contentStream)
+        {
+            var totalBytesRead = 0L;
+            var readCount = 0L;
+            var buffer = new byte[8192];
+            var isMoreToRead = true;
+
+            await using var fileStream = new FileStream(_destinationFilePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true);
+            do
+            {
+                var bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    isMoreToRead = false;
+                    TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+                    continue;
+                }
+
+                await fileStream.WriteAsync(buffer, 0, bytesRead);
+
+                totalBytesRead += bytesRead;
+                readCount += 1;
+
+                if (readCount % 100 == 0)
+                    TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+            }
+            while (isMoreToRead);
+        }
+
+        private void TriggerProgressChanged(long? totalDownloadSize, long totalBytesRead)
+        {
+            if (ProgressChanged == null)
+                return;
+
+            double? progressPercentage = null;
+            if (totalDownloadSize.HasValue)
+                progressPercentage = Math.Round((double)totalBytesRead / totalDownloadSize.Value * 100, 2);
+
+            ProgressChanged(totalDownloadSize, totalBytesRead, progressPercentage);
+        }
+
+        public void Dispose() => _httpClient?.Dispose();
+    }
+}
