8a39dc749ed1b1fdb2a201aebb19237be94ec056
Yuuki Wesp <ls-micro@ya.ru>
2020-08-27 08:19:59 +0300

    add Emoji support

---
+++ src/tools/nekoc/StringEx.cs
@@ -0,0 +1,14 @@
+﻿namespace nekoc
+{
+    using System;
+
+    public static class StringEx
+    {
+        public static string Emoji(this string str)
+        {
+            if (Environment.GetEnvironmentVariable("EMOJI_USE") == "0")
+                return "";
+            return EmojiOne.EmojiOne.ShortnameToUnicode(str);
+        }
+    }
+}
