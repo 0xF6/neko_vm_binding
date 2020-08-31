848a8e5b72f579190123288bb371431e7767e5c2
Yuuki Wesp <ls-micro@ya.ru>
2020-09-01 00:26:33 +0300

    add AsDirectoryInfo, AsFileInfo

--- src/tools/nekoc/StringEx.cs
+++ src/tools/nekoc/StringEx.cs
@@ -1,6 +1,7 @@
 ﻿namespace nekoc
 {
     using System;
+    using System.IO;
 
     public static class StringEx
     {
@@ -8,5 +9,8 @@
             Environment.GetEnvironmentVariable("EMOJI_USE") == "0"
                 ? ""
                 : EmojiOne.EmojiOne.ShortnameToUnicode(str);
+
+        public static DirectoryInfo AsDirectoryInfo(this string str) => new (str);
+        public static FileInfo AsFileInfo(this string str) => new (str);
     }
 }
