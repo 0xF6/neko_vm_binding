554fecc3f175a03e1b3b8c9e1494e6b21fde08d4
Yuuki Wesp <ls-micro@ya.ru>
2020-09-05 01:19:19 +0300

    add __unsafe_cast (box, unbox pointer)

---
+++ src/vm/native/__unsafe_cast.cs
@@ -0,0 +1,18 @@
+﻿namespace Neko.NativeRing
+{
+    using Neko.Base;
+    using System.Reflection;
+    internal static unsafe class __unsafe_cast
+    {
+        public static void* _cdel(object o)
+        {
+            if (o is NekoBehaviour no)
+                return no.@ref;
+            return Pointer.Unbox(o);
+        }
+        public static object _cwel(void* o) => Pointer.Box(o, typeof(void*));
+
+        public static void* _cmp(MethodInfo info, params object[] args) => _cdel(info.Invoke(null, args));
+        public static void _cmv(MethodInfo info, params object[] args) => info.Invoke(null, args);
+    }
+}
