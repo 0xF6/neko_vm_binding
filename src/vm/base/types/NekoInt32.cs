fc526802a9ebaefdf9402044cd5ec7d9e639ec31
Yuuki Wesp <ls-micro@ya.ru>
2020-09-07 02:03:06 +0300

    fucking int32, add NekoInt

--- src/vm/base/types/NekoInt32.cs
+++ src/vm/base/types/NekoInt32.cs
@@ -21,5 +21,26 @@
             new NekoInt32(Native.neko_alloc_int32(i));
 
         public int Value => this;
+
+        public override string ToString() => Value.ToString();
+    }
+    public sealed unsafe class NekoInt : NekoObject
+    {
+        internal NekoInt(NekoValue* value) : base(value) 
+            => NekoAssert.IsInt(value);
+
+        public static implicit operator int(NekoInt i)
+        {
+            if (NekoType.is_null(i.@ref))
+                throw new NullReferenceNekoException<int>(i.@ref);
+            return ((((int) (IntPtr) (i.@ref)) >> 1));
+        }
+
+        public static implicit operator NekoInt(int i) => 
+            new NekoInt(Native.neko_alloc_int(i));
+
+        public int Value => this;
+
+        public override string ToString() => Value.ToString();
     }
 }
