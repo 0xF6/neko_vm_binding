a703742dfe45955be0edf3ef4095acf2748bb0db
Yuuki Wesp <ls-micro@ya.ru>
2020-09-09 22:27:27 +0300

    add GetEnumerator for range

--- src/vm/extensions/RangeEx.cs
+++ src/vm/extensions/RangeEx.cs
@@ -8,5 +8,7 @@
     {
         public static IEnumerable<T> ForEach<T>(this Range range, Func<int,  T> functor) 
             => Enumerable.Range(range.Start.Value, range.End.Value - range.Start.Value).Select(functor);
+        public static IEnumerator<int> GetEnumerator(this Range range) 
+            => Enumerable.Range(range.Start.Value, range.End.Value - range.Start.Value).GetEnumerator();
     }
 }
